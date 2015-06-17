b880e881b5e0f497506bbb4430ef88b95a471241
Mark Clearwater <csmacnz@csmac.co.nz>
2015-06-18 08:48:34 +1200

    Added NotNull struct

---
+++ src/Beefeater.Tests/NotNullTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BCLExtensions;
+using Beefeater.Tests.TestHelpers;
+using Xunit;
+
+namespace Beefeater.Tests
+{
+    public class NotNullTests
+    {
+        [Fact]
+        public void ValidFooConstructsNotNullCorrectly()
+        {
+            var foo = new Foo();
+
+            var item = CreateNotNull(foo);
+
+            Assert.Equal(foo, GetValue(item));
+        }
+
+        [Fact]
+        public void NullReferenceThrowsException()
+        {
+            Foo foo = null;
+
+            Func<Foo, NotNull<Foo>> action = CreateNotNull;
+            Assert.Throws<PanicException>(action.AsActionUsing(foo).AsThrowsDelegate());
+        }
+
+        [Fact]
+        public void DefaultNotNullThrowsWhenValueIsAccessed()
+        {
+            var notNullFoo = default(NotNull<Foo>);
+
+            Func<NotNull<Foo>, Foo> action = GetValue;
+            Assert.Throws<PanicException>(action.AsActionUsing(notNullFoo).AsThrowsDelegate());
+        }
+
+        private static NotNull<Foo> CreateNotNull(Foo foo)
+        {
+            var item = new NotNull<Foo>(foo);
+            return item;
+        }
+
+        private static Foo GetValue(NotNull<Foo> item)
+        {
+            return item.Value;
+        }
+
+        public class Foo
+        {
+        }
+    }
+}
---
+++ src/Beefeater/NotNull.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Beefeater
+{
+    public struct NotNull<T>
+    {
+        private readonly bool _hasValue;
+        private readonly T _value;
+
+        public NotNull(T value) : this()
+        {
+            if (value == null) throw new PanicException();
+            _value = value;
+            _hasValue = true;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if(!_hasValue)throw new PanicException();
+                return _value;
+            }
+        }
+    }
+
+    public class PanicException : Exception
+    {
+    }
+}
