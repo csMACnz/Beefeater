e03ee17510e9c883131b7fc408061e24f62bb512
Mark Clearwater <csmacnz@csmac.co.nz>
2015-07-05 10:37:55 +1200

    Added Option Type

---
+++ src/Beefeater.Tests/OptionTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using BCLExtensions;
+using Beefeater.Tests.TestHelpers;
+using Xunit;
+
+namespace Beefeater.Tests
+{
+    public class OptionTests
+    {
+        public class WhenAnOptionWithAValidFooIsConstructedThen
+        {
+            private readonly Foo _foo;
+            private readonly Option<Foo> _option;
+
+            public WhenAnOptionWithAValidFooIsConstructedThen()
+            {
+                _foo = new Foo();
+
+                _option = CreateOption(_foo);
+            }
+
+            [Fact]
+            public void OptionHasValueIsTrue()
+            {
+                Assert.True(_option.HasValue);
+            }
+
+            [Fact]
+            public void OptionValueMatchesOriginalFoo()
+            {
+                Assert.Equal(_foo, GetValue(_option));
+            }
+        }
+
+        public class WhenCreatingAnOptionFooUsingNullThen
+        {
+            private readonly Option<Foo> _option;
+
+            public WhenCreatingAnOptionFooUsingNullThen()
+            {
+                _option = new Option<Foo>(null);
+            }
+
+            [Fact]
+            public void OptionHasValueIsTrue()
+            {
+                Assert.False(_option.HasValue);
+            }
+
+            [Fact]
+            public void GetValueThrowsException()
+            {
+                Func<Option<Foo>, Foo> action = GetValue;
+                Assert.Throws<PanicException>(action.AsActionUsing(_option).AsThrowsDelegate());
+            }
+        }
+
+        public class WhenCreatingANoneOfOptionFooThen
+        {
+            private readonly Option<Foo> _option;
+
+            public WhenCreatingANoneOfOptionFooThen()
+            {
+                _option = Option<Foo>.None;
+            }
+
+            [Fact]
+            public void OptionHasValueIsTrue()
+            {
+                Assert.False(_option.HasValue);
+            }
+
+            [Fact]
+            public void GetValueThrowsException()
+            {
+                Func<Option<Foo>, Foo> action = GetValue;
+                Assert.Throws<PanicException>(action.AsActionUsing(_option).AsThrowsDelegate());
+            }
+        }
+
+        public class WhenAnOptionWithAnIntIsConstructedThen
+        {
+            private readonly int _value;
+            private readonly Option<int> _option;
+
+            public WhenAnOptionWithAnIntIsConstructedThen()
+            {
+                _value = 42;
+
+                _option = CreateOption(_value);
+            }
+
+            [Fact]
+            public void OptionHasValueIsTrue()
+            {
+                Assert.True(_option.HasValue);
+            }
+
+            [Fact]
+            public void OptionValueMatchesOriginalFoo()
+            {
+                Assert.Equal(_value, GetValue(_option));
+            }
+        }
+
+        public class WhenCreatingANoneOfOptionIntThen
+        {
+            private readonly Option<int> _option;
+
+            public WhenCreatingANoneOfOptionIntThen()
+            {
+                _option = Option<int>.None;
+            }
+
+            [Fact]
+            public void OptionHasValueIsTrue()
+            {
+                Assert.False(_option.HasValue);
+            }
+
+            [Fact]
+            public void GetValueThrowsException()
+            {
+                Func<Option<int>, int> action = GetValue;
+                Assert.Throws<PanicException>(action.AsActionUsing(_option).AsThrowsDelegate());
+            }
+        }
+
+        private static T GetValue<T>(Option<T> item)
+        {
+            return item.Value;
+        }
+
+        private static Option<T> CreateOption<T>(T foo)
+        {
+            return new Option<T>(foo);
+        }
+
+        public class Foo
+        {
+        }
+    }
+}
---
+++ src/Beefeater/Option.cs
@@ -0,0 +1,39 @@
+﻿namespace Beefeater
+{
+    public struct Option<T>
+    {
+        private readonly T _value;
+        private readonly bool _hasValue;
+
+        public Option(T value) : this()
+        {
+            if (value != null)
+            {
+                _hasValue = true;
+                _value = value;
+            }
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (!_hasValue)
+                {
+                    throw new PanicException();
+                }
+                return _value;
+            }
+        }
+
+        public bool HasValue
+        {
+            get { return _hasValue; }
+        }
+
+        public static Option<T> None
+        {
+            get { return new Option<T>(); }
+        }
+    }
+}
