8f539f19d51191f402def8281f68afdd73f9876b
Mark Clearwater <csmacnz@csmac.co.nz>
2015-07-08 21:37:35 +1200

    Migrated Result across from another project.

---
+++ src/Beefeater.Tests/ResultTests.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using BCLExtensions;
+using Beefeater.Tests.TestHelpers;
+using Xunit;
+
+namespace Beefeater.Tests
+{
+    public class ResultTests
+    {
+        public class ProvidedValidString
+        {
+            private const string TestResult = "My Result";
+            private readonly Result<string, Exception> _result;
+
+            public ProvidedValidString()
+            {
+                const string result = TestResult;
+                _result = new Result<string, Exception>(result);
+            }
+
+            [Fact]
+            public void ConstructsSuccessfully()
+            {
+                Assert.NotNull(_result);
+            }
+
+            [Fact]
+            public void IsSuccessful()
+            {
+                Assert.True(_result.Successful);
+            }
+
+            [Fact]
+            public void ReturnsInputResult()
+            {
+                Assert.Equal(TestResult, _result.Value);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessException()
+            {
+                AssertThrowsException<Exception, InvalidOperationException>(_result, GetException);
+            }
+        }
+
+        public class ProvidedNullString
+        {
+            private readonly Result<string, Exception> _result;
+
+            public ProvidedNullString()
+            {
+                const string result = null;
+
+                _result = new Result<string, Exception>(result);
+            }
+
+            [Fact]
+            public void ConstructsSuccessfully()
+            {
+                Assert.NotNull(_result);
+            }
+
+            [Fact]
+            public void IsSuccessful()
+            {
+                Assert.True(_result.Successful);
+            }
+
+            [Fact]
+            public void ReturnsInputResult()
+            {
+                Assert.Equal(null, _result.Value);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessException()
+            {
+                AssertThrowsException<Exception, InvalidOperationException>(_result, GetException);
+            }
+        }
+
+        public class ProvidedException
+        {
+            private readonly Result<string, Exception> _result;
+
+            public ProvidedException()
+            {
+                Exception result = new Exception();
+                _result = new Result<string, Exception>(result);
+            }
+
+            [Fact]
+            public void ConstructsSuccessfully()
+            {
+                Assert.NotNull(_result);
+            }
+
+            [Fact]
+            public void IsNotSuccessful()
+            {
+                Assert.False(_result.Successful);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessResult()
+            {
+                AssertThrowsException<string, InvalidOperationException>(_result, GetResult);
+            }
+        }
+
+        [Fact]
+        public void ProvidedNullExceptionThrows()
+        {
+            Exception exception = null;
+
+            Func<Exception, Result<string, Exception>> function = CreateResultFrom;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(function.AsActionUsing(exception).AsThrowsDelegate());
+        }
+
+        private static Result<string, Exception> CreateResultFrom(Exception exception)
+        {
+            return new Result<string, Exception>(exception);
+        }
+
+        public class WhenConstructedUsingDefault
+        {
+            private readonly Result<string, Exception> _result;
+
+            public WhenConstructedUsingDefault()
+            {
+                _result = default(Result<string, Exception>);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessSuccessful()
+            {
+                AssertThrowsException<bool, PanicException>(_result, GetSuccessful);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessResult()
+            {
+                AssertThrowsException<string, PanicException>(_result, GetResult);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessException()
+            {
+                AssertThrowsException<Exception, PanicException>(_result, GetException);
+            }
+        }
+
+        public class WhenConstructedUsingDefaultConstructor
+        {
+            private readonly Result<string, Exception> _result;
+
+            public WhenConstructedUsingDefaultConstructor()
+            {
+                _result = new Result<string, Exception>();
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessSuccessful()
+            {
+                AssertThrowsException<bool, PanicException>(_result, GetSuccessful);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessResult()
+            {
+                AssertThrowsException<string, PanicException>(_result, GetResult);
+            }
+
+            [Fact]
+            public void ThrowsWhenYouAccessException()
+            {
+                AssertThrowsException<Exception, PanicException>(_result, GetException);
+            }
+        }
+
+
+        private static void AssertThrowsException<T, TException>(Result<string, Exception> result, Func<Result<string, Exception>, T> action) where TException : Exception
+        {
+            Assert.Throws<TException>(action.AsActionUsing(result).AsThrowsDelegate());
+        }
+
+        private static Exception GetException(Result<string, Exception> result)
+        {
+            return result.Exception;
+        }
+
+        private static string GetResult(Result<string, Exception> result)
+        {
+            return result.Value;
+        }
+
+        private static bool GetSuccessful(Result<string, Exception> result)
+        {
+            return result.Successful;
+        }
+    }
+}
---
+++ src/Beefeater/Result.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Beefeater
+{
+    /// <summary>
+    /// Represents a result that is either successful or has an exception.
+    /// </summary>
+    /// <typeparam name="TResult">The result, on success.</typeparam>
+    /// <typeparam name="TException">The Exception, on failure.</typeparam>
+    public struct Result<TResult, TException>
+    {
+        private readonly TException _exception;
+        private readonly TResult _result;
+        private readonly bool _successful;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Result{TResult, TException}"/> class.
+        /// </summary>
+        /// <param name="result">The result on success.</param>
+        public Result(TResult result)
+        {
+            _successful = true;
+            _result = result;
+            _exception = default(TException);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Result{TResult, TException}"/> class.
+        /// </summary>
+        /// <param name="error">The error on failure.</param>
+        public Result(TException error)
+        {
+            if(error == null) throw new ArgumentNullException("error");
+            _successful = false;
+            _exception = error;
+            _result = default(TResult);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="Result{TResult, TException}"/> is successful.
+        /// </summary>
+        public bool Successful
+        {
+            get
+            {
+                if (!_successful && _exception == null)
+                {
+                    throw new PanicException();
+                }
+                return _successful;
+            }
+        }
+
+        /// <summary>
+        /// Gets the result.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">Result cannot be accessed on unsuccessful Result.</exception>
+        public TResult Value
+        {
+            get
+            {
+                if (_successful) return _result;
+                if (_exception == null)
+                {
+                    throw new PanicException();
+                }
+                throw new InvalidOperationException("Result cannot be accessed on unsuccessful Result.");
+            }
+        }
+
+        /// <summary>
+        /// Gets the exception.
+        /// </summary>
+        /// <exception cref="System.InvalidOperationException">Exception cannot be accessed on unsuccessful Result.</exception>
+        public TException Exception
+        {
+            get
+            {
+                if (_successful)
+                    throw new InvalidOperationException("Exception cannot be accessed on unsuccessful Result.");
+                if (_exception != null) return _exception;
+                throw new PanicException();
+            }
+        }
+    }
+}
