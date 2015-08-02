f0f853ae3400179f43a734d4ac522bc516909559
Mark Clearwater <csmacnz@csmac.co.nz>
2015-08-03 07:54:12 +1200

    fixed missing coveralls tools folder path

--- build.tasks.ps1
+++ build.tasks.ps1
@@ -58,7 +58,7 @@ task LocalTestSettings {
 }
 
 task ResolveCoverallsPath {
-    $script:coveralls = (Resolve-Path "src/packages/coveralls.net.*/csmacnz.coveralls.exe").ToString()
+    $script:coveralls = (Resolve-Path "src/packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
 }
 
 task AppVeyorEnvironmentSettings {
